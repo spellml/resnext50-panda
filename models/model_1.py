297bd53a0c1f9e807fe0f95c42f26aa52e4ebc2b
Aleksey Bilogur <aleksey@spell.ml>
2020-12-11 21:02:48 +0000

    Add train-test split.

--- models/model_1.py
+++ models/model_1.py
@@ -28,50 +28,32 @@ OUT_TRAIN = '/mnt/prostate-cancer-grade-assessment/train.zip'
 OUT_MASKS = '/mnt/prostate-cancer-grade-assessment/masks.zip'
 NUM_EPOCHS = 16
 BATCH_SIZE = 16
+TRAIN_TEST_SPLIT = 0.8
 
 
 ##############
 # DATALOADER #
 ##############
 
-def estimate_mean_and_std(zf_path, n_samples=1000):
-    def calculate_mean_and_std(img_arr):
-        return np.mean(img_arr, axis=(0, 1)), np.std(img_arr, axis=(0, 1))
+def get_train_test_split():
+    # NOTE(aleksey): the DataFrame contains 100 entries which are mysteriously absent from
+    # the train set. Here we filter these out here.
+    with zipfile.ZipFile(OUT_TRAIN) as zf_train:
+        files = [item.filename for item in zf_train.infolist()]
+        files = [f[:32] for f in files]
+        files = set(files)
 
-    all_per_channel_means = []
-    all_per_channel_stds = []
-    
-    with zipfile.ZipFile(zf_path) as zf:
-        for img_info in np.random.choice(zf.infolist(), n_samples):
-            img_fn = img_info.filename
-            with zf.open(img_fn) as fp:
-                img = Image.open(io.BytesIO(fp.read()))
-                img_arr = np.array(img)
-                per_channel_means, per_channel_stds = calculate_mean_and_std(img_arr)
-                all_per_channel_means.append(per_channel_means)
-                all_per_channel_stds.append(per_channel_stds)
-    
-    estimated_channelwide_means = np.stack(all_per_channel_means).mean(axis=0)
-    estimated_channelwide_stds = np.stack(all_per_channel_stds).mean(axis=0)
-    return estimated_channelwide_means, estimated_channelwide_stds
-
-tile_img_tensor_means, tile_img_tensor_stds = estimate_mean_and_std(OUT_TRAIN)
-tile_mask_tensor_mean, tile_mask_tensor_std = estimate_mean_and_std(OUT_MASKS)
+        df = pd.read_csv(LABELS).set_index('image_id')
+        df = df.loc[files]
+    df = df.reset_index()
+    train_df = df.sample(len(df) // TRAIN_TEST_SPLIT)
+    test_df = df.loc[df.index.difference(train_df.index)]
+    return train_df, test_df
 
 
 class PandaDataset(Dataset):
-    def __init__(self):
-        # NOTE(aleksey): the DataFrame contains 100 entries which are mysteriously absent from
-        # the train set. Here we filter these out by hand.
-        with zipfile.ZipFile(OUT_TRAIN) as zf_train:
-            files = [item.filename for item in zf_train.infolist()]
-            files = [f[:32] for f in files]
-            files = set(files)
-
-            df = pd.read_csv(LABELS).set_index('image_id')
-            df = df.loc[files]
-        self.df = df.reset_index()
-
+    def __init__(self, df):
+        self.df = df
         self.image_zf = zipfile.ZipFile(OUT_TRAIN)
         self.mask_zf = zipfile.ZipFile(OUT_MASKS)
 
@@ -121,12 +103,36 @@ class PandaDataset(Dataset):
 # MODEL #
 #########
 
-# NOTE(aleksey):
-# The model uses a Mish() module as part of its architecture. In the original Kaggle kernel this
-# is sourced from the mish_activation package. I could not determine the origin of this module,
-# but inspecting the module in a Kaggle notebook (via import mish_activation; mish_activation??)
-# reveals that the activation definition is a simple flat file. I've gone ahead and copied that in
-# here.
+# NOTE(aleksey): these magic values are used by torchvision.transforms.Normalize.
+def estimate_mean_and_std(zf_path, n_samples=1000):
+    def calculate_mean_and_std(img_arr):
+        return np.mean(img_arr, axis=(0, 1)), np.std(img_arr, axis=(0, 1))
+
+    all_per_channel_means = []
+    all_per_channel_stds = []
+    
+    with zipfile.ZipFile(zf_path) as zf:
+        for img_info in np.random.choice(zf.infolist(), n_samples):
+            img_fn = img_info.filename
+            with zf.open(img_fn) as fp:
+                img = Image.open(io.BytesIO(fp.read()))
+                img_arr = np.array(img)
+                per_channel_means, per_channel_stds = calculate_mean_and_std(img_arr)
+                all_per_channel_means.append(per_channel_means)
+                all_per_channel_stds.append(per_channel_stds)
+    
+    estimated_channelwide_means = np.stack(all_per_channel_means).mean(axis=0)
+    estimated_channelwide_stds = np.stack(all_per_channel_stds).mean(axis=0)
+    return estimated_channelwide_means, estimated_channelwide_stds
+
+tile_img_tensor_means, tile_img_tensor_stds = estimate_mean_and_std(OUT_TRAIN)
+tile_mask_tensor_mean, tile_mask_tensor_std = estimate_mean_and_std(OUT_MASKS)
+
+# NOTE(aleksey): the model uses a Mish() module as part of its architecture. In the original
+# Kaggle kernel this model is sourced from the mish_activation package. I could not determine
+# the origin of this module, but inspecting the module in a Kaggle notebook (via import
+# mish_activation; mish_activation??) reveals that the activation definition is a simple flat
+# file. I've gone ahead and copied that to here.
 class MishFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x):
@@ -199,18 +205,22 @@ class PandaModel(nn.Module):
 
 writer = SummaryWriter(f'/spell/tensorboards/model_1')
 
+train_df, test_df = get_train_test_split()
+train_dataset, test_dataset = PandaDataset(train_df), PandaDataset(test_df)
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+
 model = PandaModel()
 model.cuda()
-
 optimizer = optim.Adam(model.parameters())
 criterion = nn.CrossEntropyLoss()
-dataset = PandaDataset()
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE)
 
 for epoch in range(1, NUM_EPOCHS + 1):
+    # train
     losses = []
-
-    for i, (X, _, y) in enumerate(iter(dataloader)):
+    model.train()
+    
+    for i, (X, _, y) in enumerate(iter(train_dataloader)):
         optimizer.zero_grad()
 
         X = X.cuda()
@@ -224,7 +234,7 @@ for epoch in range(1, NUM_EPOCHS + 1):
         curr_loss = loss.item()
         if i % 10 == 0:
             print(
-                f'Finished epoch {epoch}, batch {i}. Loss: {curr_loss:.3f}.'
+                f'Finished training epoch {epoch}, batch {i}. Loss: {curr_loss:.3f}.'
             )
 
         writer.add_scalar(
@@ -233,9 +243,37 @@ for epoch in range(1, NUM_EPOCHS + 1):
         losses.append(curr_loss)
 
     print(
-        f'Finished epoch {epoch}. '
+        f'Finished training epoch {epoch}. '
         f'avg loss: {np.mean(losses)}; median loss: {np.min(losses)}'
     )
+    
     if not os.path.exists('/spell/checkpoints/'):
         os.mkdir('/spell/checkpoints/')
     torch.save(model.state_dict(), f'/spell/checkpoints/model_{epoch}.pth')
+    
+    # validation
+    losses = []
+    model.eval()
+    
+    with torch.no_grad():
+        for i, (X, _, y) in enumerate(iter(test_dataloader)):
+            X = X.cuda()
+            y = y.cuda()
+            
+            X_pred = model(X)
+            loss = criterion(X_pred, y)
+            curr_loss = loss.item()
+            if i % 10 == 0:
+                print(
+                    f'Finished eval epoch {epoch}, batch {i}. Loss: {curr_loss:.3f}.'
+                )
+            
+            writer.add_scalar(
+                'validation loss', curr_loss, epoch * len(dataloader) + i
+            )
+            losses.append(curr_loss)
+
+        print(
+            f'Finished training epoch {epoch}. '
+            f'avg loss: {np.mean(losses)}; median loss: {np.min(losses)}'
+        )
